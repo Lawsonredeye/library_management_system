1fc4584ffc74dad291080bfbd58ad613c93dc678
lawsonredeye <omoregbeeolawson@gmail.com>
2025-05-27 23:09:46 +0100

    Enhance error handling in main function by logging errors during book updates

--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/lawsonredeye/lms/internal/repository"
 	"github.com/lawsonredeye/lms/internal/service"
@@ -11,10 +12,14 @@ func main() {
 	db := repository.NewBookDatabase()
 	bookService := service.NewBookService(db)
 
-
 	id := bookService.CreateBook("1984", "George Orwell", 1949, "Dystopian")
 	fmt.Println("Book created with ID:", id)
 
-	bookService.PrintBooks()
+	if err := bookService.UpdateBookByID(id, "The great wall of china", "", 0, ""); err != nil {
+		log.Fatal(err)
+	}
 
+	d, _ := bookService.GetBookByID(id)
+	log.Println(d)
+	bookService.PrintBooks()
 }
