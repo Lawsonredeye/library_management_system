758fe13ed34e76c800c59407b8b4517656e5f0a5
lawsonredeye <omoregbeeolawson@gmail.com>
2025-05-29 14:58:36 +0100

    feat: modeling book ledger for keeping track of borrowed books

--- internal/domain/books.go
+++ internal/domain/books.go
@@ -6,6 +6,12 @@ import (
 	"github.com/lawsonredeye/lms/pkg"
 )
 
+const (
+	BORROWED        string = "borrowed"
+	RETURNED        string = "returned"
+	ERRBOOKNOTFOUND string = "book with id not found"
+)
+
 type Book struct {
 	ID            string    `json:"id"`
 	Title         string    `json:"title"`
@@ -14,6 +20,14 @@ type Book struct {
 	Genre         string    `json:"genre"`
 	CreatedAt     time.Time `json:"created_at"`
 	UpdatedAt     time.Time `json:"updated_at"`
+}
+
+type BookLedger struct {
+	BookID    string
+	LenderID  string
+	Status    string
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
 
 func NewBook(title, author string, publishedYear int, genre string) *Book {
@@ -27,5 +41,16 @@ func NewBook(title, author string, publishedYear int, genre string) *Book {
 		Genre:         genre,
 		CreatedAt:     now,
 		UpdatedAt:     now,
+	}
+}
+
+func NewBorrowedBook(id, lenderId string) *BookLedger {
+	now := time.Now()
+	return &BookLedger{
+		BookID:    id,
+		LenderID:  lenderId,
+		Status:    BORROWED,
+		CreatedAt: now,
+		UpdatedAt: now,
 	}
 }
